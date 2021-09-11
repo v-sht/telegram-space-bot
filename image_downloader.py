10ff46d66729cb7dcfb3eef1d16ddbed460a53eb
Vasiliy Shtykovskiy <v.sht1987@gmail.com>
2021-09-11 12:26:37 +0300

    add params in function arguments

--- image_downloader.py
+++ image_downloader.py
@@ -1,7 +1,7 @@
 import requests
 
-def get_image(url, filepath):
-    response = requests.get(url)
+def get_image(url, filepath, params=None):
+    response = requests.get(url, params=params)
     response.raise_for_status()
 
     with open(filepath, 'wb') as file:
