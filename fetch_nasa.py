2ec7f409a3a5b1569656add1ed2e0dc210696278
Vasiliy Shtykovskiy <v.sht1987@gmail.com>
2021-09-11 10:34:07 +0300

    set payload parameters for url

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -8,9 +8,14 @@ from image_downloader import get_image
 
 
 def fetch_nasa_apod(images_folder, token):
+
+    payload = {
+        'api_key': token,
+        'count': 10,
+    }
+
     response = requests.get(
-        'https://api.nasa.gov/planetary/apod?api_key={}&count=10'
-        .format(token))
+        'https://api.nasa.gov/planetary/apod', params=payload)
     response.raise_for_status()
     launches_data = response.json()
     launch_images = []
@@ -28,21 +33,29 @@ def fetch_nasa_apod(images_folder, token):
 
 
 def fetch_nasa_epic(images_folder, token):
+
+    payload = {
+        'api_key': token,
+    }
+
     response = requests.get(
-        'https://api.nasa.gov/EPIC/api/natural/images?api_key={}'
-        .format(token))
+        'https://api.nasa.gov/EPIC/api/natural/images', params=payload)
     response.raise_for_status()
     photos_data = response.json()
 
     for image_number, photo_data in enumerate(photos_data, start=1):
         image_date = photo_data['date']
         image_date_datetime = datetime.datetime.fromisoformat(image_date)
+
+        payload = {
+            'api_key': token,
+        }
+
         image_url = 'https://api.nasa.gov/EPIC/archive/natural/{0}/{1}/{2}/png/{3}.png?api_key={4}'.format(
             image_date_datetime.year,
             image_date_datetime.strftime('%m'),
             image_date_datetime.strftime('%d'),
-            photo_data['image'],
-            token)
+            photo_data['image'])
         image_name = 'epic{}.png'.format(image_number)
         filepath = os.path.join(images_folder, image_name)
 
